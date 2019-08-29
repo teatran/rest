3d03d8fa3f37cf90ed3cc6497a46a208c5388e95
teatran <teaprogramming@gmail.com>
2019-08-29 16:25:40 +0700

    Second commit

--- src/main/java/com/example/demo/rest/UserRegistrationRestController.java
+++ src/main/java/com/example/demo/rest/UserRegistrationRestController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,15 +37,43 @@ public class UserRegistrationRestController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> getUserById(@PathVariable("id") final Long id) {
-		UserDTO user = userJpaRepository.findById(id).orElse(new UserDTO());
+		UserDTO user = userJpaRepository.findById(id).orElse(null);
+		if (user == null) {
+			return new ResponseEntity<UserDTO>(HttpStatus.NOT_FOUND);
+		}
 		return new ResponseEntity<UserDTO>(user, HttpStatus.OK);
 	}
 	
 	@PostMapping(value="/", consumes=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserDTO> createUser(@RequestBody final UserDTO user) {
+		if (userJpaRepository.findByName(user.getName()) != null) {
+			return new ResponseEntity<UserDTO>(HttpStatus.CONFLICT);
+		}
 		userJpaRepository.save(user);
 		return new ResponseEntity<UserDTO>(user, HttpStatus.CREATED);
 	}
 	
-	// PUT
+	@PutMapping(value="/{id}", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserDTO> updateUser(
+			@PathVariable("id") final Long id, @RequestBody final UserDTO editedUser) {
+		UserDTO user = userJpaRepository.findById(id).orElse(null);
+		if (user == null) {
+			return new ResponseEntity<UserDTO>(HttpStatus.NOT_FOUND);
+		}
+		user.setName(editedUser.getName());
+		user.setAddress(editedUser.getAddress());
+		user.setEmail(editedUser.getEmail());
+		userJpaRepository.saveAndFlush(user);
+		return new ResponseEntity<UserDTO>(user, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<UserDTO> deleteUser(@PathVariable("id") final Long id) {
+		UserDTO user = userJpaRepository.findById(id).orElse(null);
+		if (user == null) {
+			return new ResponseEntity<UserDTO>(HttpStatus.NOT_FOUND);
+		}
+		userJpaRepository.delete(user);
+		return new ResponseEntity<UserDTO>(HttpStatus.NO_CONTENT);
+	}
 }
--- src/main/resources/static/js/mycontroller.js
+++ src/main/resources/static/js/mycontroller.js
@@ -11,9 +11,5 @@ app.controller('listuserController', function ($scope, $http, NgTableParams) {
 		$scope.users = response.data;
 		self.tableParams = new NgTableParams({}, { dataset: response.data});
 	});
-	var data = [{"id":1,"name":"tea","address":"Sóc Trăng","email":"tea@gmail"},
-		{"id":2,"name":"tea","address":"Sóc Trăng","email":"david@gmail"},
-		{"id":3,"name":"code","address":"Sóc Trăng","email":"code@gmail"}
-	];
 	
 });
