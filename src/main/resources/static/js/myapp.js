9d40defb9c8036b6323388ac4bd48921ad58748a
teatran <teaprogramming@gmail.com>
2019-08-30 14:56:55 +0700

    Completing Chap 3

--- src/main/resources/static/js/myapp.js
+++ src/main/resources/static/js/myapp.js
@@ -1,13 +1,16 @@
 var app = angular.module('myapp', ['ngRoute', 'ngResource', 'ngTable']);
 
 app.config(function ($routeProvider) {
-	$routeProvider.when('/listuser', {
-		templateUrl: '/template/listuser.html',
+	$routeProvider.when('/list-users', {
+		templateUrl: '/template/list-users.html',
 		controller: 'listuserController',
 		controllerAs: 'vm'
-	}). when('/register', {
-		templateUrl: '/template/register',
+	}). when('/register-user', {
+		templateUrl: '/template/register-user.html',
 		controller: 'registerController'
+	}). when('/update-user/:id', {
+		templateUrl: '/template/update-user.html',
+		controller: 'userController'
 	}).otherwise({
 		redirectTo: '/home',
 		templateUrl: '/template/home.html'
--- src/main/resources/static/js/mycontroller.js
+++ src/main/resources/static/js/mycontroller.js
@@ -1,15 +1,58 @@
-app.controller('registerController', function () {
+app.controller('registerController', function ($scope, $http, $location, $route) {
+	$scope.submitUserForm = function () {
+		$http({
+			method: 'POST',
+			url: 'http://localhost:8080/api/user/',
+			data: $scope.user
+		}).then(function success(response) {
+			$location.path('/list-users');
+			$route.reload();
+		}, function error(response) {
+			$scope.errorMessage = 'Error: ' + response.status;
+		});
+	};
+	
+	$scope.resetForm = function() {
+		$scope.user = null;
+	};
 	
 });
 
-app.controller('listuserController', function ($scope, $http, NgTableParams) {
+app.controller('listuserController', function ($scope, $http, $location, NgTableParams) {
 	var self = this;
 	$http({
 		method: 'GET',
 		url: 'http://localhost:8080/api/user/'
-	}).then(function (response) {
+	}).then(function success(response) {
 		$scope.users = response.data;
 		self.tableParams = new NgTableParams({}, { dataset: response.data});
 	});
 	
+	// click event for Edit button
+	$scope.editUser = function(userId) {
+		$location.path('/update-user/' + userId);
+	}
+});
+
+app.controller('userController', function ($scope, $http, $location, $routeParams, $route) {
+	// get user by id
+	$http({
+		method: 'GET',
+		url: 'http://localhost:8080/api/user/' + $routeParams.id
+	}).then(function success(response) {
+		$scope.user = response.data;
+	});
+	
+	$scope.submitUserForm = function () {
+		$http({
+			method: 'PUT',
+			url: 'http://localhost:8080/api/user/' + $routeParams.id,
+			data: $scope.user
+		}).then(function success(response) {
+			$location.path('/list-users');
+			$route.reload();
+		}, function error(response) {
+			$scope.errorMessage = 'Erorr updating user';
+		});
+	}
 });
